bf6a8489199c796a9819f31e0c11499be7a96fd5
Janielton Medeiros <janieltonmedeiros@outlook.com>
2023-10-06 11:38:51 -0300

    atualização minimina

--- curriculo/src/main/java/com/serratec/curriculo/controller/CurriculoController.java
+++ curriculo/src/main/java/com/serratec/curriculo/controller/CurriculoController.java
@@ -1,6 +1,6 @@
 package com.serratec.curriculo.controller;
 
-import java.io.File;
+//import java.io.File;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +19,7 @@ import com.serratec.curriculo.model.Curriculo;
 import com.serratec.curriculo.service.CurriculoService;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
+//import io.swagger.annotations.ApiImplicitParam;
 
 
 
--- curriculo/src/main/java/com/serratec/curriculo/model/Curriculo.java
+++ curriculo/src/main/java/com/serratec/curriculo/model/Curriculo.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
