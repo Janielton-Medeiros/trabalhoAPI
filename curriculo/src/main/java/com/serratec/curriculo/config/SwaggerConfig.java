fac0c47a2d040d9fabf538e0d4d003ed0d3b8597
EduardoAngelo1 <eduardoangelo300@gmail.com>
2023-10-06 17:55:23 -0300

    adicionando comentarios

--- curriculo/src/main/java/com/serratec/curriculo/config/SwaggerConfig.java
+++ curriculo/src/main/java/com/serratec/curriculo/config/SwaggerConfig.java
@@ -12,20 +12,24 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+// Anotação indicando que esta classe é uma configuração para o Spring
 @EnableWebMvc
 public class SwaggerConfig {
 	
     @Bean
-    public Docket api() {
+    //Esse objeto será gerenciado pelo Spring e pode ser injetado em outras partes do seu aplicativo que precisam dele. É uma maneira de configurar e criar objetos de forma centralizada no contexto do Spring.
+    public Docket api() {// Inicializa um novo Docket usando o tipo de documentação SWAGGER_2
         return new Docket(DocumentationType.SWAGGER_2)
+         // Configura os pacotes que devem ser escaneados para endpoints da API
           .select()
           .apis(RequestHandlerSelectors.basePackage("com.serratec.curriculo"))
           .paths(PathSelectors.any())
           .build()
+           // Configura as informações da API, como título, descrição e versão
           .apiInfo(apiInfo());
     }
     
-    private ApiInfo apiInfo() {
+    private ApiInfo apiInfo() { // Método privado para configurar as informações da API
         return new ApiInfoBuilder()
                 .title("Auto Magicamente")
                 .description("Documentação da API da minha aplicação Spring Boot")
--- curriculo/src/main/java/com/serratec/curriculo/repository/CurriculoRepository.java
+++ curriculo/src/main/java/com/serratec/curriculo/repository/CurriculoRepository.java
@@ -18,22 +18,28 @@ import com.serratec.curriculo.model.FotoPerfil;
 
 @Repository
 public class CurriculoRepository {
-	
+
+	//Especificar o caminho para pasta
+    //Raiz é usado para definir o caminho para a raiz da pasta de curriculos.
     @Value("${serratec.curriculo.raiz}")
     private String raiz;
+    //O valor da propriedade diretorioFotos é usado para definir o caminho para a pasta de fotos de perfil dos curriculos.
     @Value("${serratec.curriculo.diretorio-fotos}")
     private String diretorioFotos;
     
+    
     FotoPerfil fp = new FotoPerfil();
     
-    private List<Curriculo> curriculos = new ArrayList<>();
+    private List<Curriculo> curriculos = new ArrayList<>();//Armazena o curriculo como se fosse um banco de dados
     
     private long ultimoId = 1;
 
+    // Obter todos os curriculos
     public List<Curriculo> ObterTodos(){
         return curriculos;
     }
 
+    // Obter um curriculo pelo ID
     public Curriculo ObterCurriculo(long id){
 
         Curriculo curriculoEncontrado = null;
@@ -46,13 +52,16 @@ public class CurriculoRepository {
         } 
         return curriculoEncontrado;
     }
-
+    
+    // Atualizar um curriculo
     public Curriculo Adicionar(Curriculo curriculo){
     	
-        
+        // Define o ID do currículo para o valor da propriedade `ultimoId`.
         curriculo.setId(ultimoId);
+        // Salva a foto de perfil do currículo e a imagem base64 da foto de perfil.
         curriculo.setFotoPerfil(fp.getDiretorio());
         curriculo.setImagemBase64(fp.getBase64());
+        // Adiciona o currículo à lista de currículos.
         curriculos.add(curriculo);
         ultimoId++;
         return curriculo;
@@ -60,14 +69,17 @@ public class CurriculoRepository {
 
     public Curriculo Atualizar(Curriculo curriculo){
 
+         // Remover o curriculo da lista de curriculos
         curriculos.removeIf(c -> c.getId() == curriculo.getId());
 
+        // Adicionar o curriculo atualizado à lista de curriculos
         curriculos.add(curriculo);
 
         return curriculo;
     }
 
     public void Deletar(Long id){
+        // Deletar um curriculo pelo ID
         curriculos.removeIf(c -> c.getId() == id);
     }
 
@@ -76,13 +88,15 @@ public class CurriculoRepository {
     	
     	//String nomeFoto = Long.toString(ultimoId);
     	
+         // Criar um caminho para a imagem de perfil
     	Path diretorioPath = Paths.get(this.raiz, this.diretorioFotos);
 		Path arquivoPath = diretorioPath.resolve(Long.toString(ultimoId).concat(".jpg"));
-		FileInputStream imageInputStream = null;
+		FileInputStream imageInputStream = null;//pega a umagem e transforma de byte
 
-		try {      
+		try {   
+            // Criar a pasta para a imagem de perfil, se ela não existir   
 			Files.createDirectories(diretorioPath);
-			arquivo.transferTo(arquivoPath.toFile());
+			arquivo.transferTo(arquivoPath.toFile()); // Salvar a imagem de perfil
 			fp.setDiretorio(arquivoPath.toString());
 			fp.setId(ultimoId); 
 			fp.setNome(Long.toString(ultimoId));
@@ -91,17 +105,21 @@ public class CurriculoRepository {
 			throw new RuntimeException("Problemas na tentativa de salvar arquivo.", e);
 		}
 		
-		try {
-			imageInputStream = new FileInputStream(arquivoPath.toString());
-			byte[] imageBytes;
+		try {//Salva a imagem de perfil do currículo em um arquivo de texto com o nome do ID do currículo seguido de `imgBase64.txt`
+			// Lê a imagem de perfil como uma matriz de bytes.
+            imageInputStream = new FileInputStream(arquivoPath.toString());
+			byte[] imageBytes; 
 			imageBytes = new byte[imageInputStream.available()];
 			imageInputStream.read(imageBytes);
+
+             // Converte a matriz de bytes para uma string base64.
 			String base64Image = Base64.getEncoder().encodeToString(imageBytes);
 			
-			
+			// Salva a string base64 em um arquivo de texto.
 			try (FileWriter writer = new FileWriter(diretorioPath+"/"+Long.toString(ultimoId)+"imgBase64.txt")) {
 				writer.write(base64Image);
 			}
+            // Define o nome do arquivo base64 na propriedade `base64` do objeto `FotoPerfil`.
 			fp.setBase64(Long.toString(ultimoId)+"imgBase64.txt");
 			
 		} catch (IOException e) {
